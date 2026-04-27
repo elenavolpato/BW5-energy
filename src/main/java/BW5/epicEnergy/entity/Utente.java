777cece9bd46e1ac6a440647624bdb8269fa3bd8
Dedes <federico.lepore73@gmail.com>
2026-04-27 12:10:17 +0200

    Utente entities

--- src/main/java/BW5/epicEnergy/entity/Utente.java
+++ src/main/java/BW5/epicEnergy/entity/Utente.java
@@ -1,4 +1,52 @@
 package BW5.epicEnergy.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "utenti")
 public class Utente {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cognome;
+
+    @Column(name = "avatar_url", nullable = false)
+    private String avatarURL;
+
+    public Utente(String username, String email, String password, String nome, String cognome) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.avatarURL = "https://ui-avatars.com/api?name=" + nome + "+" + cognome;
+    }
+
+    
 }
