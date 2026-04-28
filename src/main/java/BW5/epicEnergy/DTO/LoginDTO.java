f6c4abea165e14593d935cf545b3f92ea07b4583
Dedes <federico.lepore73@gmail.com>
2026-04-28 14:46:26 +0200

    LoginDTO LoginRespDTO NewUtenteRespDTO

---
+++ src/main/java/BW5/epicEnergy/DTO/LoginDTO.java
@@ -0,0 +1,4 @@
+package BW5.epicEnergy.DTO;
+
+public record LoginDTO(String email, String password) {
+}
---
+++ src/main/java/BW5/epicEnergy/DTO/NewUtenteRespDTO.java
@@ -0,0 +1,6 @@
+package BW5.epicEnergy.DTO;
+
+import java.util.UUID;
+
+public record NewUtenteRespDTO(UUID userId) {
+}
