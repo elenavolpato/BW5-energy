5b0b9b72035ef3172db542aca18e075046db8e1f
Dedes <federico.lepore73@gmail.com>
2026-04-28 12:26:51 +0200

    TokenTools

---
+++ src/main/java/BW5/epicEnergy/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package BW5.epicEnergy.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
