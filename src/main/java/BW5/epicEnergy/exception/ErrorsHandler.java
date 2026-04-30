b04386513e757bf838ba0b4762d2a51c4b8686e4
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-30 16:14:49 +0200

    Adds some exception handlers

--- src/main/java/BW5/epicEnergy/exception/ErrorsHandler.java
+++ src/main/java/BW5/epicEnergy/exception/ErrorsHandler.java
@@ -2,10 +2,15 @@ package BW5.epicEnergy.exception;
 
 import BW5.epicEnergy.DTO.ErrorDTO;
 import BW5.epicEnergy.DTO.ErrorsListDTO;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.core.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
@@ -33,5 +38,49 @@ public class ErrorsHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDTO handleNotFoundException(NotFoundException ex) {
         return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ErrorDTO("Not valid input provided", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorDTO handleUnauthorizedException(UnauthorizedException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorDTO handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return new ErrorDTO("Access denied, you don't have the required permission", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return new ErrorDTO("Not valid input provided", LocalDateTime.now());
+    }
+
+
+    @ExceptionHandler(PropertyReferenceException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handlePropertyReferenceException(PropertyReferenceException ex) {
+        return new ErrorDTO("Not valid search param", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return new ErrorDTO("Deletion of the desired resource avoided", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleGenericException(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorDTO("Oops, a server error occurred!", LocalDateTime.now());
     }
 }
